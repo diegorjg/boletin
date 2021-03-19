5f554b2db5e84d06aa9c736ce2cdc1e5c029d285
diegorjg <52799845+diegorjg@users.noreply.github.com>
2021-03-18 20:37:41 -0500

    Add files via upload

---
+++ boletin/models.py
@@ -0,0 +1,22 @@
+
+
+
+from django.db import models
+
+
+# Create your models here.
+
+class Registrado(models.Model):
+   nombre = models.CharField(max_length=100, blank=True, null=True)
+   email = models.EmailField()
+   timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+   def _str_(self):
+
+   	   return self.email
+  	
+
+  
+ 
+    
+
+
